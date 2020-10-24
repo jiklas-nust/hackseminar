10e32b4bd73689087d889d11367a5f4e2f6ae0a6
jiklas-nust <justniklas1@gmail.com>
2020-10-24 18:00:47 +0200

    Hello World Programm

---
+++ main.py
@@ -0,0 +1,6 @@
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Use a breakpoint in the code line below to debug your script.
+# Press the green button in the gutter to run the script.
+
+print('Hello World')
