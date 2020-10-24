e31d5ae90960fe07129fc73ae48d28fbcddfeb00
jiklas-nust <justniklas1@gmail.com>
2020-10-24 18:55:13 +0200

    Hello World Message und andere

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+print('')
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # Use a breakpoint in the code line below to debug your script.
 # Press the green button in the gutter to run the script.
 
-print('Hello World')
-print('Felix ist komischer Kautz ein')
-print('Hallo Julia')
+greeting = 'Hello'
+name = 'Felix'
+
+message = f'{greeting}, {name.upper()}. Du Spacko!'
+
+print(message)
+
+num = 6
+
+remainder = num % 2
+
+if remainder == 1:
+    print(f'Die Zahl {num} ist ungerade')
+else:
+    print(f'Die Zahl {num} ist gerade')
